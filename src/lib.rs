5c69f8ef10303767a451233654813eb11c06a931
SpectralPixel <encrypteddvjjrxv@gmail.com>
2024-04-18 20:48:52 +0200

    Implemented insertion functions from tutorial unit tests will follow shortly

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,15 @@ pub struct Tree {
     root: Option<Node>,
 }
 
+impl Tree {
+    pub fn insert(&mut self, node: Node) {
+        match &mut self.root {
+            Some(r) => {
+                r.insert(node);
+            },
+            None => {
+                self.root = Some(node);
+            }
+        }
+    }
+}
--- src/node.rs
+++ src/node.rs
@@ -12,4 +12,19 @@ impl Node {
             right: None,
         }
     }
+
+    pub fn insert(&mut self, node: Node) {
+        use std::cmp::Ordering::*;
+        match node.data.cmp(&self.data) {
+            Equal => *self = node,
+            Less => match &mut self.left {
+                Some(n) => { n.insert(node); },
+                None => { self.left = Some(Box::new(node)); }
+            },
+            Greater => match &mut self.right {
+                Some(n) => { n.insert(node); }
+                None => { self.right = Some(Box::new(node)); }
+            },
+        }
+    }
 }
